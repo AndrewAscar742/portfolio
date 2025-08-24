3d40ab34cd8a1835d5196a82e4544bf67c7853de
AndrewAscar742 <andrewmatosascar@outlook.com>
2025-08-24 18:15:12 -0300

    feature: criação da rota portfolio conectando no portfolio.component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { PortfolioComponent } from './features/portfolio/portfolio.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'portfolio' // redireciona a rota raiz para /portfolio
+  },
+  {
+    path: 'portfolio',
+    component: PortfolioComponent
+  }
+];
